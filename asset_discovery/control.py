9cf15d8adcc84afd6cfe62869bf70d6608ed81ac
sadiye Buyukcukurlu <sadiyebuyukcukurlu@gmail.com>
2020-11-25 12:12:51 +0300

    over

--- asset_discovery/control.py
+++ asset_discovery/control.py
@@ -1,15 +1,10 @@
-import sys, ipaddress,nmap
+import nmap 
 
-class checkConn:
-    def checkNmap(host):
-       IPAddress = host
-       hostscan = nmap.PortScanner()
-       parsed_ip = IPAddress[0:10]
-       
-       x=0
-       while x < 256:
-        s=parsed_ip+str(x)
-        results = hostscan.scan(hosts=s, arguments='-T4')
-        x+=1
-        cikti=results['scan']
-        return(cikti)
+class checkControl: 
+
+    def checkNmap(host): 
+        IPAddress = host 
+        hostscan = nmap.PortScanner() 
+        results = hostscan.scan(hosts=IPAddress+'/24', arguments='-T4') 
+        end=results['scan'] 
+        reSturn(end) 
--- asset_discovery/discovery.py
+++ asset_discovery/discovery.py
@@ -1,15 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Resource, Api
-from control import control
+from control import checkControl
 
 app = Flask(__name__)
 api = Api(app)
 
 class nmapCheck(Resource):
     def get(self, IPAddress):
-       return(checkConn.checkNmap(IPAddress))
+       return(checkControl.checkNmap(IPAddress))
 
-api.add_resource(nmapCheck, '/nmap/<string:IPAddress>/')
+api.add_resource(nmapCheck, '/nmap/<string:IPAddress>')
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.129', port='5000', debug=True)
+    app.run(host='127.0.0.1', port='5000', debug=True)
