a51346196feb893d5582b8cf1ec96c375acdaeb8
sadiye Buyukcukurlu <sadiyebuyukcukurlu@gmail.com>
2020-11-26 10:55:37 +0300

    over

--- asset_discovery/control.py
+++ asset_discovery/control.py
@@ -1,10 +1,11 @@
 import nmap 
+#Nmap: Bağlantı noktası tarayıcısıdır.
 
 class checkControl: 
 
-    def checkNmap(host): 
-        IPAddress = host 
-        hostscan = nmap.PortScanner() 
-        results = hostscan.scan(hosts=IPAddress+'/24', arguments='-T4') 
-        end=results['scan'] 
-        return(end) 
+    def checkNmap(host):  #Nmap i kontrol edecek bir fonksiyon yazıp içine parametre eklenir.
+        IPAddress = host  #Discovery.py da girdi olarak alınan IPAddress i buradaki host a eşitlenir.
+        results = nmap.PortScanner().scan(hosts=IPAddress+'/24', arguments='-T4') #Girilen ip nin bulunduğu network üzerindeki tüm ip lere erişip portlarını taratıp result a eşitlenir.
+        #Scan ın içine Girilen ip nin bulunduğu network üzerindeki tüm ip lere erişebilmek için /24 eklenir. Arguments olarak daha hızlı calısması için nmap in komutu olan -T4 verilir.
+        end=results['scan'] #Çıkan sonuçlarin içinden sadece scan in içeriğini bir değişkene atanir.
+        return(end)       #Sadece end değişkeninin dönmesini sağlanır.
--- asset_discovery/discovery.py
+++ asset_discovery/discovery.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
+#Flask :api dir. ‘uygulama programlama arayüzü’ Flask-RESTful :REST API'lerini hızla oluşturmak için destek ekleyen bir Flask uzantısıdır.
+#Flask-RESTful kütüphanesinin içinden resource ve Api import edilir.
 from flask_restful import Resource, Api
-from control import checkControl
+from control import checkControl #Control dosyası içinden checkControl class ı import edilir.
 
-app = Flask(__name__)
+app = Flask(__name__) #_ name _ değişkeni dosyanın (modülünün) hangi yolla çağrıldığını kontrol eder.
 api = Api(app)
 
-class nmapCheck(Resource):
-    def get(self, IPAddress):
-       return(checkControl.checkNmap(IPAddress))
+class nmapCheck(Resource): #İçerisinde resource olan bir class oluşturulur.
+    def get(a, IPAddress): #Get metodunu kullanarak fonksiyon oluşturup 2 parametre verilir.
+       return(checkControl.checkNmap(IPAddress)) #Control dosyasından getirilen checkControl class ının içinden checkNmap fonksiyonunu çağırarak ip adresini döndürülür.
 
-api.add_resource(nmapCheck, '/nmap/<string:IPAddress>')
+api.add_resource(nmapCheck, '/nmap/<string:IPAddress>')#Tanımlanmış olan apinin içinden add_resource alınıp , nmapCheck class ının içinde dönecek olan ip adresinin girdisi alınır.
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='5000', debug=True)
+if __name__ == '__main__': # kontrolü dışarıdan import edildiğinde hangi alanların çalıştırılacağının seçimini yapar.
+    app.run(host='127.0.0.1', port='5000', debug=True) #Hangi ip ve port üstünden bu bilgilere erişebileceğimizi belirlenir.
